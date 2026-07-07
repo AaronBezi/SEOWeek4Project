e6fc4d195f807f3dc488d51e1f0cf65b174152b4
Cailan Jeremiah-Barry <cailanjeremiahbarry@.com>
2026-07-07 14:58:36 -0400

    removed notes schema temporarily

--- database/models.py
+++ database/models.py
@@ -1,10 +1,17 @@
 from database import db
 from datetime import datetime
-#File used to create tables for the database
+#File used to create Schemas for the database
 
 class User(db.model):
-    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(20),nullable=False)
     time_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+
+
+
+    
+
+
