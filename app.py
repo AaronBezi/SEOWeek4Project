c41ce69c32cad0c3600fcddb9aa2a390246ad51a
dpdoescode <diegoperez9858985@outlook.com>
2026-07-07 12:49:34 -0400

    merged html/css/js to flask default template and linked register page with home page

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect, request
+# url_for allows us to find where this file is in our HTML
+from flask_behind_proxy import FlaskBehindProxy
+from forms import RegistrationForm
+import git
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+proxied = FlaskBehindProxy(app)          # handle codio redirection
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
-@app.route('/')
+@app.route("/")
 def home():
     # renders the index.html file from the templates folder
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/seoproject2/SEOWeek4Project')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
+if __name__ == '__main__':               # this should always be at the end
+    app.run(debug=True, host="0.0.0.0")
