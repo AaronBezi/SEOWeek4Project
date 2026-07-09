66da7c990bdb5a2f543d8c3f0467d76e9f1c5a9b
dpdoescode <diegoperez9858985@outlook.com>
2026-07-09 15:13:24 -0400

    made documents clickable, added function in storage.py fetching note urls and made note content-types explicit upon upload

--- storage.py
+++ storage.py
@@ -21,7 +21,14 @@ def allowed_file(filename):
 
 def upload_note_file(file_storage):
     extension = file_storage.filename.rsplit('.', 1)[1].lower()
+    content_type = file_storage.content_type
     note_id = str(uuid.uuid4())
     storage_path = f"{note_id}.{extension}"
-    get_supabase().storage.from_(BUCKET_NAME).upload(storage_path, file_storage.read())
+    get_supabase().storage.from_(BUCKET_NAME).upload(storage_path, file_storage.read(), file_options={"content-type": content_type})
     return note_id,storage_path
+
+# Retrieves the signed URL for a note file so that it can be accessed by users.
+def get_note_file(file_path):
+    expires_in_seconds = 3600  # 1 hour
+    data = get_supabase().storage.from_(BUCKET_NAME).create_signed_url(file_path, expires_in_seconds)  # retrieves a dictionary of the signed URL
+    return data['signedUrl']  # returns the actual url to the actual file bytes 
